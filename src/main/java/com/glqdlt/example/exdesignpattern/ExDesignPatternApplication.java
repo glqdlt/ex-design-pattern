2d40b1266050ba1abde7fdd3077be051ad8bce96
iw.jhun <dlfdnd0725@gmail.com>
2018-07-22 15:44:01 +0900

    init

---
+++ src/main/java/com/glqdlt/example/exdesignpattern/ExDesignPatternApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.example.exdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExDesignPatternApplication.class, args);
+    }
+}
