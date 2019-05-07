33be16d704d4dc5339d93ab2bbabe21f8dd5bc49
jhun <jhun@nm-4ones.com>
2019-05-07 17:38:04 +0900

    커링

---
+++ src/test/java/com/glqdlt/example/exdesignpattern/fpp/CurriedTest.java
@@ -0,0 +1,33 @@
+package com.glqdlt.example.exdesignpattern.fpp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.function.Function;
+
+/**
+ * @author Jhun
+ * 2019-05-07
+ */
+public class CurriedTest {
+
+    Function<Integer, Function<Integer, Integer>> aaaa = x -> y -> x + y;
+
+    Function<Integer, Function<Integer, Integer>> bbbb = (x) -> {
+        return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer) {
+                return integer + x;
+            }
+        };
+    };
+
+    @Test
+
+    public void name() {
+
+        Assert.assertEquals(3, (int) aaaa.apply(1).apply(2));
+        Assert.assertEquals(3, (int) bbbb.apply(1).apply(2));
+
+    }
+}
